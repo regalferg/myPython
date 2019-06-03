fa226a7541bc55897741bfe6813746c25b278c94
Ferguson, Charles <charles.ferguson@verizonwireless.com>
2019-06-03 15:45:10 -0400

    Open JSON and Print url, titles, alias

---
+++ jsonintro/GOTjson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""Author regalferg | Email: Nada || Python Practice"""
+
+# python has no jsonbatteries in ze box, we need to import
+import json
+
+def main():
+    ##open a file in write mode
+    with open('jonsnow.json','r') as gotdata: #'w' = write, 'r' = read', 'a' = append
+        jonsnow = gotdata.read()
+        GOTpy = json.loads(jonsnow) 
+    #print(GOTpy)
+    print(GOTpy["url"])
+    print(GOTpy["titles"][0])
+    print(GOTpy["aliases"])
+
+        #parse jonsnow file for ...
+        #display char name
+        #display char alias/titles
+        #display the API for ???
+        
+
+if __name__ == "__main__":  
+    main()
